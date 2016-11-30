e524384b6cc35868783b066a9559c85a7970be76
Auðunn Ezra Kristinsson Simpson <FS453488@fss.is>
2016-11-30 09:56:06 +0000

    uppfærði homepage og footer.nýtt localhost port er 3001 (debug)

--- components/Footer.js
+++ components/Footer.js
@@ -1,17 +1,18 @@
 import React from 'react'
-import {Col} from 'react-bootstrap'
+import { Col, LinkContainer } from 'react-bootstrap'
+import { Link } from 'react-router'
 var Footer = React.createClass({
   render: function(){
     var graenn = {backgroundColor:'green', color:'white'}
     var raudur = {backgroundColor:'red', color:'white'}
     var fjolublar = {backgroundColor:'indigo', color:'white'}
-    var blar = {backgroundColor:'blue', color:'white'}
+    var blar = {backgroundColor:'blue', color:'white', textDecoration:'none'}
     return (
       <div>
         <Col xs={6} sm={6} md={3} lg={3} style={graenn}>footerItem</Col>
         <Col xs={6} sm={6} md={3} lg={3} style={raudur}>footerItem2</Col>
         <Col xs={6} sm={6} md={3} lg={3} style={fjolublar}>footerItem3</Col>
-        <Col xs={6} sm={6} md={3} lg={3} style={blar}>footerItem4</Col>
+        <Col xs={6} sm={6} md={3} lg={3} style={blar}><a href="https://github.com/audunnezra" style={{ color:'white', textDecoration: 'none' }}>github account</a  ></Col>
       </div>
     );
   }
--- components/Homepage.js
+++ components/Homepage.js
@@ -3,12 +3,12 @@ import {Col} from 'react-bootstrap'
 
 
 var ws = {backgroundColor:'#3c3c3c', color:'white', "height":'200rem', opacity:0 }
-var mid = {color:'white', "height":'200rem'}
-var container = {"height":"100wh"}
+var mid = {color:'white', "height":'200rem', backgroundColor: '#262b33'}
+var container = {"height":"100wh",}
 
-var hgb = {"height":'200rem', backgroundColor:'#3cfadb', textAlign:'center'}
-var gbh = {"height":'200rem', backgroundColor:'indigo', textAlign:'center'}
-var bgh = {"height":'200rem', backgroundColor:'#007fad', textAlign:'center'}
+var hgb = {"height":'200rem', textAlign:'center'}
+var gbh = {"height":'200rem', textAlign:'center'}
+var bgh = {"height":'200rem', textAlign:'center'}
 var sizer = {"height":'33.3333333333rem'}
 
 var welcome = { fontFamily:'Didot LT STD', fontSize:'68px' }
@@ -20,28 +20,28 @@ var Homepage = React.createClass({
         <Col xsHidden smHidden md={1} lg={2} style={ws}>footerItem</Col>
         <Col xs={12} sm={12} md={10} lg={8} style={mid}>
             <Col xs={4} sm={4} md={2.5} lg={2.66666667} style={hgb}>
-              <div style={sizer}>haha</div>
-              <div style={sizer}>gei</div>
-              <div style={sizer}>bwa</div>
-              <div style={sizer}>bwa</div>
-              <div style={sizer}>bwa</div>
-              <div style={sizer}>bwa</div>
+              <div style={sizer}></div>
+              <div style={sizer}>doot</div>
+              <div style={sizer}></div>
+              <div style={sizer}></div>
+              <div style={sizer}></div>
+              <div style={sizer}></div>
             </Col>
             <Col xs={4} sm={4} md={2.5} lg={2.66666667} style={gbh}>
               <div style={sizer}><h2 style={welcome}>Welcome</h2></div>
-              <div style={sizer}>bwa</div>
-              <div style={sizer}>haha</div>
-              <div style={sizer}>haha</div>
-              <div style={sizer}>haha</div>
-              <div style={sizer}>haha</div>
+              <div style={sizer}>doot</div>
+              <div style={sizer}></div>
+              <div style={sizer}></div>
+              <div style={sizer}></div>
+              <div style={sizer}></div>
             </Col>
             <Col xs={4} sm={4} md={2.5} lg={2.66666667} style={bgh}>
-              <div style={sizer}>bwa</div>
-              <div style={sizer}>gei</div>
-              <div style={sizer}>haha</div>
-              <div style={sizer}>haha</div>
-              <div style={sizer}>haha</div>
-              <div style={sizer}>haha</div>
+              <div style={sizer}></div>
+              <div style={sizer}>doot</div>
+              <div style={sizer}></div>
+              <div style={sizer}></div>
+              <div style={sizer}></div>
+              <div style={sizer}></div>
             </Col>
         </Col>
         <Col xsHidden smHidden md={1} lg={2} style={ws}>footerItem3</Col>
